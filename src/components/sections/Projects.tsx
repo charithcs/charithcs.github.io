95148f04f200a8b5278a1ae0402dc67ed3b3d58d
Securedsoul99 <157104580+Securedsoul99@users.noreply.github.com>
2025-06-23 22:35:43 +0530

    Updated Projects.tsx

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { icons } from "lucide-react";
+import { MicIcon as icons } from "lucide-react";
 import {
   Dialog,
   DialogContent,
