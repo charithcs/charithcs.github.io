2d76b940652a54e9c7f87ff84536b99256ce8ebc
Securedsoul99 <157104580+Securedsoul99@users.noreply.github.com>
2025-06-26 13:26:42 +0530

    Fix React Router basename for subpath deployment

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/Portfolio-website-black-white">
             <Routes>
               <Route path="/" element={<Index />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
