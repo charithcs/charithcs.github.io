f2a2f39910069e2f553a4c9a7228df45bcceac89
Securedsoul99 <157104580+Securedsoul99@users.noreply.github.com>
2025-06-30 12:29:50 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Portfolio-website-black-white/",
+  base: "/Portfolio-Website/",
   server: {
     host: "::",
     port: 8080,
