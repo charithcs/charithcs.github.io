73bdb432b68a6e756b28edc7bb53e391bf074e35
Securedsoul99 <157104580+Securedsoul99@users.noreply.github.com>
2025-07-29 13:05:43 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter basename="/charith.s.github.io">
+          <BrowserRouter basename="/portfolio-website">
             <Routes>
               <Route path="/" element={<Index />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
